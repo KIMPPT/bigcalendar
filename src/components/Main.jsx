f9770bb750e9c7da480bb41dc86d527abfee5cf2
KIMPPT <dudghks9303@gmail.com>
2023-06-05 17:55:54 +0900

    06051755

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -35,10 +35,62 @@ export default function Main() {
     }),
     []
   );
+  const customDayPropGetter = (date) => {
+    let data = [
+      { day: moment("2023-02-15"), holidayType: true },
+      { day: moment("2020-02-20"), holidayType: false },
+    ];
+
+    var obj = data.find((element, index) => {
+      if (
+        (element.day === moment(date) && element.holidayType === false) ||
+        (element.day === moment(date) && element.holidayType === true)
+      )
+        return data[index];
+    });
+    console.log(obj);
+    if (
+      obj !== undefined &&
+      obj.day === moment(date) &&
+      obj.holidayType === false
+    )
+      return { style: { backgroundColor: "red" } };
+    else if (
+      obj !== undefined &&
+      obj.day === moment(date) &&
+      obj.holidayType === true
+    )
+      return { style: { backgroundColor: "orange" } };
+    else return { style: { backgroundColor: "#fff" } };
+  };
+  const dayprop = (date) => {
+    event.map((eventday) => {
+      if (
+        eventday.day.year() === moment(date).year() &&
+        eventday.day.month() === moment(date).month() &&
+        eventday.day.date() === moment(date).date()
+      )
+        return {style:{backgroundColor:"blue"}}
+        //{ style: { backgroundColor: "red" } };
+      else return {style:{backgroundColor:"red"}};
+    });
+  };
   const defaultDate = useMemo(() => now, []);
+  const customDayPropGetter2 = (date) => {
+    for(let i=0;i<event.length;i++)
+    {
+    if (date.getMonth()===moment(event[i].day).month()&&
+    date.getDate()===moment(event[i].day).date())
+        return {
+            className: 'special-day',
+            style: {backgroundColor:"Blue"},
+        };
+    else return {};
+      }
+};
   return (
     <Calendar
-      dayPropGetter={dayPropGetter}
+      dayPropGetter={customDayPropGetter2}
       defaultDate={defaultDate}
       localizer={localizer}
       style={{ height: 300, width: 300 }}
